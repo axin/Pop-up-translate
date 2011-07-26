547faa98d758b0be74e0d20f14d12a842da3d8e4
Alexey Sazikin <axinapps@gmail.com>
2011-07-26 22:33:57 +0700

    Added mockup image

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
 document.onmouseup = function(event) {
-    if(event.ctrlKey) {
-        document.getElementById("translation").innerHTML =
-        window.getSelection().toString();
+    if (event.ctrlKey) {
+        var translation = document.getElementById("translation");
+        var selection = window.getSelection();
+        var popUp = document.getElementById("pop-up");
+
+        translation.innerHTML = selection.toString();
+        //popUp.style.cssText = 'background-color: green; width: 100px;' +
+            //'height: 30px; top: 50px; left: 100px; position: absolute;';
+        
+        //debugger;
+
+        popUp.style.top = getAbsoluteOffset(selection.anchorNode.parentNode)[1] + 'px';
+        popUp.style.left = getAbsoluteOffset(selection.anchorNode.parentNode)[0] + 
+            selection.anchorOffset + 'px';
     }
 }
