83b42c93707482385add2ca0f955d039ee449b43
Alexey Sazikin <axinapps@gmail.com>
2011-08-09 23:53:16 +0700

    A lot of changes

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 var PopUp = {
-    extId: '__MSG_@@extension_id__',
+    extId: 'ipgccjoekgalifmnmoolgojgaaofkgab',
     bingAppId: 'FEA3B73DEA9F7A311924116269D3094501970811',
 
-    popUpInstance: null,
+    addedToDocument: false,
 
     Appearence: {
         fontFamily: 'sans-serif',
@@ -11,100 +11,124 @@ var PopUp = {
         backgroundColor: '#E3E3E3',
         shadowColor: '#3C81FF'
     },
-    
+
+    getStyles: function() {
+        return '#' + PopUp.extId + 'pop-up {' +
+            'position: absolute;' +
+            'z-index: 999000;' +
+            'display: none;' +
+            'left: 50px;' +
+            'top: 50px;' +
+        '}' +
+
+        '.' + PopUp.extId + 'pop-up-part {' +
+            'background-color: ' + PopUp.Appearence.backgroundColor + ';' +
+            'box-shadow: 0 0 5px 1px ' + PopUp.Appearence.shadowColor + ';' +
+        '}' +
+
+        '#' + PopUp.extId + 'buble {' +
+            'position: relative;' +
+            'float: left;' +
+            'z-index: 999100;' +
+            'border-radius: 5px;' +
+            'box-shadow: none;' +
+        '}' +
+
+        '#' + PopUp.extId + 'buble-shadow {' +
+            'position: relative;' +
+            'float: left;' +
+            'border-radius: 5px;' +
+        '}' +
+
+        '#' + PopUp.extId + 'translation-container {' +
+            'float: left;' +
+            'min-width: 2em;' +
+            'min-height: 2em;' +
+            'max-width: 500px;' +
+            'margin-right: 30px;' +
+        '}' +
+
+        '#' + PopUp.extId + 'pronounce-icon-container {' +
+            'width: 20px;' +
+            'min-width: 20px;' +
+            'min-height: 20px;' +
+            'position: absolute;' +
+            'right: 0;' +
+            'top: 50%;' +
+            'padding: 0 5px;' +
+            'margin-top: -10px;' +
+        '}' +
+
+        '#' + PopUp.extId + 'translation {' +
+            'margin: 0.5em;' +
+            'color: ' + PopUp.Appearence.fontColor + ';' +
+            'font-family: ' + PopUp.Appearence.fontFamily + ';' +
+            'font-size: ' + PopUp.Appearence.fontSize + ';' +
+        '}' +
+
+        '.' + PopUp.extId + 'arrow {' +
+            'width: 20px;' +
+            'height: 20px;' +
+            'position: absolute;' +
+            'left: 50%;' +
+            'margin-left: -10px;' +
+            '-webkit-transform: rotate(45deg) skew(10deg, 10deg);' +
+        '}' +
+
+        '#' + PopUp.extId + 'arrow-up {' +
+            'top: 0;' +
+            'margin-top: -5px;' +
+        '}' +
+
+        '#' + PopUp.extId + 'arrow-down {' +
+            'bottom: 0;' +
+            'margin-bottom: -5px;' +
+        '}';
+    },
+
     addToDocument: function() {
-        if (PopUp.popUpInstance == null) {
+        if (PopUp.addedToDocument == false) {
+            PopUp.addedToDocument = true;
+
+            var styles = document.createTextNode(PopUp.getStyles());
+
+            var styleElement = document.createElement('style');
+            styleElement.setAttribute('type', 'text/css');
+            styleElement.appendChild(styles);
+            document.head.appendChild(styleElement);
+
             PopUp.popUpInstance = document.createElement('div');
-            with (PopUp.popUpInstance.style) {
-                position = 'absolute';
-                zIndex = '999000';
-                display = 'none';
-            }
             PopUp.popUpInstance.setAttribute('id', PopUp.extId + 'pop-up');
 
             var bubleShadow = document.createElement('div');
-            with (bubleShadow.style) {
-                position = 'relative';
-                float = 'left';
-                borderRadius = '5px';
-                boxShadow = '0 0 5px 1px ' + PopUp.Appearence.shadowColor;
-            }
+            bubleShadow.setAttribute('id', PopUp.extId + 'buble-shadow');
+            bubleShadow.setAttribute('class', PopUp.extId + 'pop-up-part');
 
             var buble = document.createElement('div');
-            with (buble.style) {
-                position = 'relative';
-                float = 'left';
-                zIndex = '999100';
-                borderRadius = '5px';
-                backgroundColor = PopUp.Appearence.backgroundColor;
-            }
+            buble.setAttribute('id', PopUp.extId + 'buble');
+            buble.setAttribute('class', PopUp.extId + 'pop-up-part');
 
             var translationContainer = document.createElement('div');
-            with (translationContainer.style) {
-                minWidth = '2em';
-                minHeight = '2em';
-                maxWidth = '500px';
-                marginRight = '30px';
-            }
+            translationContainer.setAttribute('id', PopUp.extId + 'translation-container');
 
             var translation = document.createElement('p');
-            with (translation.style) {
-                margin = '0.5em';
-                color = PopUp.Appearence.fontColor;
-                fontFamily = PopUp.Appearence.fontFamily;
-                fontSize = PopUp.Appearence.fontSize;
-                translation.setAttribute('id', PopUp.extId + 'translation');
-            }
+            translation.setAttribute('id', PopUp.extId + 'translation');
 
             var pronounceIconContainer = document.createElement('div');
-            with (pronounceIconContainer.style) {
-                width = '20px';
-                minWidth = '20px'; 
-                minHeight = '20px';
-                position = 'absolute';
-                right = '0';
-                top = '50%';
-                padding = '0 5px';
-                marginTop = '-10px';
-            }
+            pronounceIconContainer.setAttribute('id', PopUp.extId + 'pronounce-icon-container');
 
             var pronounceIcon = document.createElement('img'); 
-            with (pronounceIcon) {
-                setAttribute('src', 'img/pronounce.png');
-                setAttribute('width', '20px');
-                setAttribute('height', '20px');
-                setAttribute('id', PopUp.extId + 'pronounce-icon');
-            }
+            pronounceIcon.setAttribute('src', 'img/pronounce.png');
+            pronounceIcon.setAttribute('width', '20px');
+            pronounceIcon.setAttribute('height', '20px');
 
             var arrowUp = document.createElement('div');
-            with (arrowUp.style) {
-                width = '20px';
-                height = '20px';
-                position = 'absolute';
-                left = '50%';
-                webkitTransform = 'rotate(45deg) skew(10deg, 10deg)';
-                backgroundColor = PopUp.Appearence.backgroundColor;
-                boxShadow = '0 0 5px 1px ' + PopUp.Appearence.shadowColor;
-                marginLeft = '-10px';
-                top = '0';
-                marginTop = '-5px';
-            }
             arrowUp.setAttribute('id', PopUp.extId + 'arrow-up');
+            arrowUp.setAttribute('class', PopUp.extId + 'pop-up-part arrow');
 
             var arrowDown = document.createElement('div');
-            with (arrowDown.style) {
-                width = arrowUp.style.width;
-                height = arrowUp.style.height;
-                position = arrowUp.style.position;
-                left = arrowUp.style.left;
-                webkitTransform = arrowUp.style.webkitTransform;
-                backgroundColor = arrowUp.style.backgroundColor;
-                boxShadow = arrowUp.style.boxShadow;
-                marginLeft = arrowUp.style.marginLeft;
-                bottom = '0';
-                marginBottom = '-5px';
-            }
             arrowDown.setAttribute('id', PopUp.extId + 'arrow-down');
+            arrowDown.setAttribute('class', PopUp.extId + 'pop-up-part arrow');
 
             PopUp.popUpInstance.appendChild(bubleShadow);
             bubleShadow.appendChild(buble);
@@ -118,23 +142,23 @@ var PopUp = {
         }
     },
 
+    // This function triggers when window's onclick event occures.
+    // It shows/hides pop-up depending if some text is selected or not.
     toggle: function(e) {
         var popUp = document.getElementById(PopUp.extId + 'pop-up');
         var selection = window.getSelection().toString();
 
         if (selection != '') {
             popUp.style.display = 'block';
+
+            var tranlationTextNode = document.createTextNode(selection);
+            var translationElement = document.getElementById(PopUp.extId + 'translation');
+            translationElement.appendChild(tranlationTextNode);
         }
         else {
             popUp.style.display = 'none';
         }
 
-    },
-
-    hide: function() {
-        if (PopUp.popUpInstance.style.display == 'block') {
-            PopUp.popUpInstance.style.display = 'none';
-        }
     }
 }
 
@@ -158,5 +182,4 @@ var PopUp = {
 
 window.addEventListener('load', PopUp.addToDocument, false);
 window.addEventListener('click', PopUp.toggle, false);
-//window.addEventListener('mousedown', PopUp.hide, false);
 
